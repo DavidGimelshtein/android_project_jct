fe3012fb37360edde4c8981ade7bcaaa1aa2594a
david <david.gimelshtein@gmail.com>
2016-11-22 12:11:18 +0200

    Stage 1... entities, factory, backend, datasource

---
+++ app/src/main/java/com/example/davidalex/postadeal/model/datasource/ListDsManager.java
@@ -0,0 +1,65 @@
+package com.example.davidalex.postadeal.model.datasource;
+
+import android.content.ContentValues;
+
+import com.example.davidalex.postadeal.model.backend.IDSManager;
+import com.example.davidalex.postadeal.model.entities.Activity;
+import com.example.davidalex.postadeal.model.entities.Business;
+import com.example.davidalex.postadeal.model.entities.User;
+
+import java.util.List;
+
+/**
+ * Created by david on 22.11.2016.
+ */
+
+public class ListDsManager implements IDSManager {
+
+    private List<User> userList;
+    private List<Business> businessList;
+    private List<Activity> activityList;
+
+
+
+
+
+    @Override
+    public void addUser(ContentValues userContent) {
+
+    }
+
+    @Override
+    public void addBusiness(ContentValues businessContent) {
+
+    }
+
+    @Override
+    public void addActivity(ContentValues activityContent) {
+
+    }
+
+    @Override
+    public Boolean checkChanges() {
+        return null;
+    }
+
+    @Override
+    public List<User> getUsersList() {
+        return null;
+    }
+
+    @Override
+    public List<Business> getBusinessList() {
+        return null;
+    }
+
+    @Override
+    public List<Activity> getActivitiesList() {
+        return null;
+    }
+
+    @Override
+    public void reportChanges() {
+
+    }
+}
---
+++ app/src/main/java/com/example/davidalex/postadeal/model/entities/User.java
@@ -0,0 +1,13 @@
+package com.example.davidalex.postadeal.model.entities;
+
+/**
+ * Created by david on 22.11.2016.
+ */
+
+public class User {
+
+    private int userID;
+    private String userName;
+    private String userPassword;
+
+}
