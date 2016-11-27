e1f4ed97f6b49c9ddaa9185a020d75e9748f7fad
david <david.gimelshtein@gmail.com>
2016-11-27 14:46:53 +0200

    Stage 3...

--- app/src/main/java/com/example/davidalex/postadeal/controller/MainActivity.java
+++ app/src/main/java/com/example/davidalex/postadeal/controller/MainActivity.java
@@ -11,29 +11,72 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.davidalex.postadeal.R;
+import com.example.davidalex.postadeal.model.backend.IDSManager;
 import com.example.davidalex.postadeal.model.datasource.CustomContentProvider;
+import com.example.davidalex.postadeal.model.entities.Categories;
 import com.example.davidalex.postadeal.model.entities.User;
 
-import static com.example.davidalex.postadeal.model.datasource.ListDsManager.MY_LOG;
+//import static com.example.davidalex.postadeal.model.datasource.ListDsManager.MY_LOG;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ContentValues dataContent;
+    private Uri uri;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-
         dataContent = new ContentValues();
-        addUser();
-        addUser();
-        Cursor cursor = getContentResolver().query(CustomContentProvider.USER_CONTENT_URI, null, null, null,null);
-        //с курсором проблема, класс ListDsManager функция getUsersList не может конвертировать
-        //arrylist в курсор, похоже
- //       printCursorToLog(cursor);
+        //inserting  new User
+        dataContent.put(CustomContentProvider.USER_NAME, "David");
+        dataContent.put(CustomContentProvider.USER_PASSWORD, "DGjhjkgl845");
+        dataContent.put(CustomContentProvider.USER_ID, "1");
+        uri = getContentResolver().insert(CustomContentProvider.USER_CONTENT_URI, dataContent);
+        //inserting  new User
+        dataContent.put(CustomContentProvider.USER_NAME, "Alex");
+        dataContent.put(CustomContentProvider.USER_PASSWORD, "A64646ldx");
+        dataContent.put(CustomContentProvider.USER_ID, "2");
+        uri = getContentResolver().insert(CustomContentProvider.USER_CONTENT_URI, dataContent);
+        //inserting  new Business
+        dataContent.put(CustomContentProvider.COMPANY_NAME, "IsraTour");
+        dataContent.put(CustomContentProvider.COMPANY_ADRESS, "Tel Aviv Jabotinsky 27");
+        dataContent.put(CustomContentProvider.COMPANY_TEL, "03-5555999");
+        dataContent.put(CustomContentProvider.COMPANY_EMAIL, "isratour@gmal.com");
+        dataContent.put(CustomContentProvider.COMPANY_WEB_SITE, "isratour.co.il");
+        dataContent.put(CustomContentProvider.COMPANY_ID, "1");
+        uri = getContentResolver().insert(CustomContentProvider.BUSINESS_CONTENT_URI, dataContent);
+        //inserting  new Business
+        dataContent.put(CustomContentProvider.COMPANY_NAME, "Ambasador");
+        dataContent.put(CustomContentProvider.COMPANY_ADRESS, "Jerusalem Yaffo 27");
+        dataContent.put(CustomContentProvider.COMPANY_TEL, "03-5555999");
+        dataContent.put(CustomContentProvider.COMPANY_EMAIL, "isratour@gmal.com");
+        dataContent.put(CustomContentProvider.COMPANY_WEB_SITE, "isratour.co.il");
+        dataContent.put(CustomContentProvider.COMPANY_ID, "2");
+        uri = getContentResolver().insert(CustomContentProvider.BUSINESS_CONTENT_URI, dataContent);
+        //inserting  new Activity
+        dataContent.put(CustomContentProvider.ACTIVITY_CATEGORY, Categories.HOTELS.toString());
+        dataContent.put(CustomContentProvider.ACTIVITY_COMPANY_ID, "2");
+        dataContent.put(CustomContentProvider.ACTIVITY_DESCRIPTION, "3 night at Jerusalem Ramada hotel");
+        dataContent.put(CustomContentProvider.ACTIVITY_PRICE, "1200");
+        dataContent.put(CustomContentProvider.ACTIVITY_PLACEMENT, "Israel");
+        dataContent.put(CustomContentProvider.ACTIVITY_START_DATE, "26.12.16");
+        dataContent.put(CustomContentProvider.ACTIVITY_END_DATE, "29.12.16");
+        uri = getContentResolver().insert(CustomContentProvider.ACTIVITY_CONTENT_URI, dataContent);
+        //get user list
+        cursor = getContentResolver().query(CustomContentProvider.USER_CONTENT_URI, null, null, null, null);
+        //get businesses list
+        cursor = getContentResolver().query(CustomContentProvider.BUSINESS_CONTENT_URI, null, null, null, null);
+        //get activities list
+        cursor = getContentResolver().query(CustomContentProvider.ACTIVITY_CONTENT_URI, null, null, null, null);
+        //checking if there is a new Activities
+
+        //checking if there is a new Activities
+
+
     }
 
     @Override
@@ -44,35 +87,36 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    void printCursorToLog(Cursor c){
-        if (c != null) {
-            if (c.moveToFirst()) {
-                String str;
-                do {
-                    str = "";
-                    for (String cn : c.getColumnNames()) {
-                        str = str.concat(cn + " = "
-                                + c.getString(c.getColumnIndex(cn)) + "; ");
+//    void printCursorToLog(Cursor c){
+//        if (c != null) {
+//            if (c.moveToFirst()) {
+//                String str;
+//                do {
+//                    str = "";
+//                    for (String cn : c.getColumnNames()) {
+//                        str = str.concat(cn + " = "
+//                                + c.getString(c.getColumnIndex(cn)) + "; ");
+//
+//                    }
+//                    Log.d(MY_LOG, str);
+//
+//                } while (c.moveToNext());
+//            }
+//            c.close();
+//        } else
+//            Log.d(MY_LOG, "Cursor is null");
+//    }
 
-                    }
-                    Log.d(MY_LOG, str);
-
-                } while (c.moveToNext());
-            }
-            c.close();
-        } else
-            Log.d(MY_LOG, "Cursor is null");
-    }
     private void addUser() {
         // Add a new student record
-        dataContent.put(CustomContentProvider.USER_NAME,"David");
+        dataContent.put(CustomContentProvider.USER_NAME, "David");
         dataContent.put(CustomContentProvider.USER_PASSWORD, "DGjhjkgl845");
         dataContent.put(CustomContentProvider.USER_ID, "1");
 
-        Uri uri = getContentResolver().insert(CustomContentProvider.USER_CONTENT_URI , dataContent);
-        Cursor cursor = getContentResolver().query(CustomContentProvider.USER_CONTENT_URI ,null, null, null,null);
+        Uri uri = getContentResolver().insert(CustomContentProvider.USER_CONTENT_URI, dataContent);
+        Cursor cursor = getContentResolver().query(CustomContentProvider.USER_CONTENT_URI, null, null, null, null);
 
- //       Log.d(MY_LOG, uri.toString());
+        //       Log.d(MY_LOG, uri.toString());
         //Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
     }
 }
--- app/src/main/java/com/example/davidalex/postadeal/model/datasource/ListDsManager.java
+++ app/src/main/java/com/example/davidalex/postadeal/model/datasource/ListDsManager.java
@@ -2,6 +2,7 @@ package com.example.davidalex.postadeal.model.datasource;
 
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.util.Log;
 
 import com.example.davidalex.postadeal.model.backend.IDSManager;
 import com.example.davidalex.postadeal.model.entities.Activity;
@@ -18,34 +19,39 @@ import java.util.List;
 
 public class ListDsManager implements IDSManager {
 
+    public static final String MY_LOG ="my_log" ;
+
     private List<User> userList = new ArrayList<>();
     private List<Business> businessList = new ArrayList<>();
     private List<Activity> activityList = new ArrayList<>();
+
+    private static int existActivities = 0;
 
 
     @Override
     public void addUser(ContentValues userContent) {
 
-       userList.add(new User(userContent.getAsInteger(CustomContentProvider.USER_ID),
-               userContent.getAsString(CustomContentProvider.USER_NAME),
-               userContent.getAsString(CustomContentProvider.USER_PASSWORD)));
+        userList.add(new User(userContent.getAsInteger(CustomContentProvider.USER_ID),
+                    userContent.getAsString(CustomContentProvider.USER_NAME),
+                    userContent.getAsString(CustomContentProvider.USER_PASSWORD)));
+
     }
 
     @Override
     public void addBusiness(ContentValues businessContent) {
 
-       businessList.add(new Business(businessContent.getAsInteger(CustomContentProvider.COMPANY_ID),
-               businessContent.getAsString(CustomContentProvider.COMPANY_NAME),
-               businessContent.getAsString(CustomContentProvider.COMPANY_ADRESS),
-               businessContent.getAsString(CustomContentProvider.COMPANY_TEL),
-               businessContent.getAsString(CustomContentProvider.COMPANY_EMAIL),
-               businessContent.getAsString(CustomContentProvider.COMPANY_WEB_SITE)));
+        businessList.add(new Business(businessContent.getAsInteger(CustomContentProvider.COMPANY_ID),
+                businessContent.getAsString(CustomContentProvider.COMPANY_NAME),
+                businessContent.getAsString(CustomContentProvider.COMPANY_ADRESS),
+                businessContent.getAsString(CustomContentProvider.COMPANY_TEL),
+                businessContent.getAsString(CustomContentProvider.COMPANY_EMAIL),
+                businessContent.getAsString(CustomContentProvider.COMPANY_WEB_SITE)));
     }
 
     @Override
     public void addActivity(ContentValues activityContent) {
 
-        activityList.add(new Activity((Categories)activityContent.get(CustomContentProvider.ACTIVITY_CATEGORY),
+        activityList.add(new Activity(Categories.valueOf(activityContent.getAsString(CustomContentProvider.ACTIVITY_CATEGORY)),
                 activityContent.getAsString(CustomContentProvider.ACTIVITY_PLACEMENT),
                 activityContent.getAsString(CustomContentProvider.ACTIVITY_START_DATE),
                 activityContent.getAsString(CustomContentProvider.ACTIVITY_END_DATE),
@@ -56,25 +62,42 @@ public class ListDsManager implements IDSManager {
 
     @Override
     public Boolean checkChanges() {
-        return null;
+
+        if (existActivities < activityList.size())
+        {
+            int diff = activityList.size() - existActivities;
+            existActivities = activityList.size();
+            Log.d(MY_LOG, "There is " + diff + " new Activities");
+            return true;
+        }
+        Log.d(MY_LOG, "There is no new Activities");
+        return false;
     }
 
     @Override
     public Cursor getUsersList() {
-
-        return (Cursor)userList;
+        Log.d(MY_LOG, "in getUsersList in class ListDsManager");
+        for(int i = 0; i < userList.size(); i++)
+            Log.d(MY_LOG, (i + 1) + " User name: " + userList.get(i).getUserName() + "; User Password: " + userList.get(i).getUserPassword());
+        return null;
     }
 
     @Override
     public Cursor getBusinessList() {
 
-        return (Cursor)businessList;
+        Log.d(MY_LOG, "in getBusinessList in class ListDsManager");
+        for(int i = 0; i < businessList.size(); i++)
+            Log.d(MY_LOG, (i + 1) + " Company name: " + businessList.get(i).getCompanyName() + "; Company Adress: " + businessList.get(i).getCompanyAdress());
+        return null;
     }
 
     @Override
     public Cursor getActivitiesList() {
 
-        return (Cursor)activityList;
+        Log.d(MY_LOG, "in getActivitiesList in class ListDsManager");
+        for(int i = 0; i < activityList.size(); i++)
+            Log.d(MY_LOG, (i + 1) + " Activity Category: " + activityList.get(i).getActivityCategory() + "; Activity Placement: " + activityList.get(i).getActivityPlacement());
+        return null;
     }
 
     @Override
